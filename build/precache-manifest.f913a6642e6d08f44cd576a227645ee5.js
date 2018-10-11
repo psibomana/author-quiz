f4ebf455b18ffa08bed4bc88aa1f45555d0ed1cc
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2018-10-11 17:58:26 +0300

    Github Page

---
+++ build/precache-manifest.f913a6642e6d08f44cd576a227645ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "727a75ff64ea09f73dcf",
+    "url": "/static/js/main.727a75ff.chunk.js"
+  },
+  {
+    "revision": "f5fbb4da211cc573a967",
+    "url": "/static/js/1.f5fbb4da.chunk.js"
+  },
+  {
+    "revision": "727a75ff64ea09f73dcf",
+    "url": "/static/css/main.0bf0eeb2.chunk.css"
+  },
+  {
+    "revision": "b758a25989a6330becf5e31e5a7e549c",
+    "url": "/index.html"
+  }
+];

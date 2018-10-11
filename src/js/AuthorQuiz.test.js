f12f1b33b1eb657afa27bb93f1f8c681c51913fe
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2018-10-11 15:40:09 +0300

    Test Case update

--- src/js/AuthorQuiz.test.js
+++ src/js/AuthorQuiz.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 
+
+const state = {
+  data: {
+    books: ['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities', 'Hamlet', 'Macbeth', 'Romeo and Juliet'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+  },
+  highlight: 'none'
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AuthorQuiz />, div);
+  ReactDOM.render(<AuthorQuiz {...state.data}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
